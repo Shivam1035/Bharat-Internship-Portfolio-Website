5ac23de307f3d56c2e5f1207849f365a66fe8b31
Shivam Tiwari <110774112+Shivam1035@users.noreply.github.com>
2023-08-24 23:45:52 +0530

    Add files via upload

---
+++ Portfololio1/main.js
@@ -0,0 +1,31 @@
+// Header Scroll
+var typed = new Typed(".text", {
+    strings: ["Fornted Developer", "YouTuber", "Web Developer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
+
+let nav = document.querySelector(".navbar");
+window.onscroll = function () {
+    if(document.documentElement.scrollTop > 20){
+        nav.classList.add("header-scrolled");
+    }else{
+        nav.classList.remove("header-scrolled");
+    }
+} 
+
+// nav hide 
+let navBar = document.querySelectorAll(".nav-link");
+let navCollapse = document.querySelector(".navbar-collapse.collapse");
+navBar.forEach(function (a){
+    a.addEventListener("click", function(){
+        navCollapse.classList.remove("show");
+    })
+})
+
+
+ 
+
+
